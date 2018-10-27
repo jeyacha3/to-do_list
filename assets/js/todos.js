35d8dd177451b97520b403232974a38cd6072ec3
Janola Jeyachandra <JJeyach@users.noreply.github.com>
2018-10-27 11:08:00 -0400

    initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+// Check off Specific Todos By Clicking
+$("ul").on("click", "li", function() {
+    // Toggle class completed css properties when clicked
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete list item
+$("ul").on("click", "span", function(event){
+    // remove entire li that span is nested in after list item fades out
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove()
+    });
+    // prevents parent elements' click events from being triggered
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    // if enter key is pressed
+    if(event.which === 13) {
+        // store text from input into variable
+        var listItem = $(this).val();
+        $(this).val("");
+        // add new list item to to-do list
+        $("ul").append("<li><span>X</span> " + listItem + "</li>");
+    }
+});
